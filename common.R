4611bbf7e19550a55b8e6a8530f320c37d8b1e75
miguel-conde <miguelco2000@gmail.com>
2016-04-11 21:17:16 +0200

    Added coomon.R and some improvements in First_feeling.Rmd

---
+++ common.R
@@ -0,0 +1,16 @@
+# Make a submission
+makeASubmission <- function (fitModel, fileName) {
+  myPred <- predict(fitModel, tidy_test, type ="prob")
+  submission <- data.frame(ID = tidy_test$ID, TARGET = myPred$UNSATISFIED)
+  fileSub <- file.path(".", "Submissions", fileName)
+  cat(sprintf("saving the submission file %s\n", fileSub))
+  write.csv(submission, fileSub, row.names = F)
+}
+
+modelPerformance <- function(model, predictors, reference, lev, model_name) {
+  myData <- cbind(pred = predict(model, predictors), 
+                  obs = reference, 
+                  predict(model, predictors, type = "prob"))
+  twoClassSummary(data = myData, lev = lev,
+                  model = model_name)
+}
