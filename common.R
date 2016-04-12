bd8aa08727ca4c11992979237d6893c69af2a3b1
miguel-conde <miguelco2000@gmail.com>
2016-04-12 19:41:11 +0200

    More models added to ensemble

--- common.R
+++ common.R
@@ -5,6 +5,13 @@ numCVs <- 3
 makeASubmission <- function (fitModel, fileName) {
   myPred <- predict(fitModel, tidy_test, type ="prob")
   submission <- data.frame(ID = tidy_test$ID, TARGET = myPred$UNSATISFIED)
+#   fileSub <- file.path(".", "Submissions", fileName)
+#   cat(sprintf("saving the submission file %s\n", fileSub))
+#   write.csv(submission, fileSub, row.names = F)
+  writeSubFile(fileName, submission)
+}
+
+writeSubFile <- function(fileName, submission) {
   fileSub <- file.path(".", "Submissions", fileName)
   cat(sprintf("saving the submission file %s\n", fileSub))
   write.csv(submission, fileSub, row.names = F)
@@ -16,4 +23,16 @@ modelPerformance <- function(model, predictors, reference, lev, model_name) {
                   predict(model, predictors, type = "prob"))
   twoClassSummary(data = myData, lev = lev,
                   model = model_name)
+}
+
+ensembleProbs <- function (modelsList, modelEnsemble, newdata, levName) {
+  model_preds <- lapply(modelsList, predict, 
+                        newdata = newdata, type="prob")
+  model_preds <- lapply(model_preds, function(x) x[,levName])
+  model_preds <- data.frame(model_preds)
+  ens_preds   <- predict(modelEnsemble, 
+                         newdata = newdata, 
+                         type="prob")
+  model_preds$ensemble <- ens_preds
+  model_preds
 }
